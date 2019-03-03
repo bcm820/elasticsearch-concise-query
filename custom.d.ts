b57bf9011f59527f0bf207671490b2e82976b74e
bcmendoza <brian.mendoza@deciphernow.com>
2019-03-03 14:19:37 -0500

    initial TS project setup

---
+++ custom.d.ts
@@ -0,0 +1,10 @@
+// To use non-code assets with TypeScript, we need to defer the type for these imports.
+// This requires a custom.d.ts file which signifies custom type definitions.
+// Here we declare a new module for SVGs by specifying any import that ends in .svg
+// and defining the module's content as any. We could be more explicit about it being
+// a url by defining the type as string. The same concept applies to other assets
+// including CSS, SCSS, JSON and more.
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
